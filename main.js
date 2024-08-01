e94c6090c05d0a036fb51a61d3de950734d66e23
MCairns <119698506+MADirkerKMC@users.noreply.github.com>
2024-08-01 11:43:14 +0200

    Add files via upload
    Added comments to the code. Responsiveness complete. Place holders for future projects added. Small adjustments made such as resizing the image to allow for faster loading

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
+//For accordion on About Me page
 var AbAcc = document.getElementsByClassName("accordion");
+//for loop to create a click event for each accordion segment in order to drop the panel.
 for(var i = 0; i<AbAcc.length; i++)
 {
   AbAcc[i].addEventListener("click", function()
   {
     this.classList.toggle("active");
-  
-
+    
   var AbPanel = this.nextElementSibling;
   if(AbPanel.style.display === "block")
   {
@@ -18,4 +19,10 @@ for(var i = 0; i<AbAcc.length; i++)
   });
 }
 
-//=========================Guess the Word=====================//
+//=========================Resume Button=====================//
+document.getElementById('btnR').addEventListener('click', function(){
+  const resume = document.createElement('a');
+  resume.href = 'Visuals/MACairnsCV.pdf'; //the path to the file 
+  resume.download = 'MACairnsCV.pdf'; //name of file
+  resume.click(); //trigger download
+});
